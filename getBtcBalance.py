a0fcc106c5c3954dfbafd6e8b063643d6f4b1557
william <williamsmith71218@gmail.com>
2022-07-28 06:56:08 -0700

    Added get balance module

--- getBtcBalance.py
+++ getBtcBalance.py
@@ -93,14 +93,22 @@ keys = [
 ]
 
 
-def getBalance(address):
+def balance(addr):
     try:
-        request_url = API + address
-        balance = requests.get(request_url)
-        total_balance = balance.json()
-        print("Balance is {}".format(total_balance))
+        total = blockcypher.get_total_balance(addr)
+        print('Total Balance is '+ str(total))
     except Exception as e:
-        print("Error ocurred: {}".format(e))
+        # total = AddressBalance().action('btc', addr)
+        print('Exception is {}'.format(e))
+
+# def getBalance(address):
+#     try:
+#         request_url = API + address
+#         balance = requests.get(request_url)
+#         total_balance = balance.json()
+#         print("Balance is {}".format(total_balance))
+#     except Exception as e:
+#         print("Error ocurred: {}".format(e))
 
 
 def main():
@@ -109,8 +117,8 @@ def main():
         wallet = Wallet(key)
         address = wallet.address.__dict__['mainnet'].__dict__['pubaddr1']
         print("Address id {}".format(address))
-        getBalance(address)
-        time.sleep(1)
+        balance(address)
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
